08bc7e7f32d48d1fdd9f523a0be67ab23b54edc4
Sylkyu <miiyusli@gmail.com>
2023-03-18 18:10:34 +1300

    change dist type to tgz

--- update-setup.sh
+++ update-setup.sh
@@ -2,20 +2,26 @@
 
 configFiles=("config/cluster.ini" "config/cluster_token.txt")
 
+failed=false
+
 for file in ${configFiles[@]}; do
   if [ ! -f $file ]; then
     echo "Config file $file does not exist. Please create it."
-    exit 1
+    failed=true
   fi
 done
 
 if [ -z "$1" ] then
   echo "Please supply a release version e.g. v1.0.0"
+  failed=true
+fi
+
+if [ $failed ] then
   exit 1
 fi
 
-wget https://github.com/Limeyuzu/dont-starve-together-server-config/releases/download/$1/MyDediServer.zip
-unzip -o MyDediServer.zip && rm MyDediServer.zip
+wget https://github.com/Limeyuzu/dont-starve-together-server-config/releases/download/$1/MyDediServer.tgz
+tar -xvzf MyDediServer.tgz && rm MyDediServer.tgz
 
 for file in ${configFiles[@]}; do
   cp $file .klei/DoNotStarveTogether/MyDediServer
