062f7ba1eed2f841fd45832158d0e73689032732
Sylkyu <miiyusli@gmail.com>
2023-03-18 13:13:23 +1300

    initial

---
+++ MyDediServer/Master/worldgenoverride.lua
@@ -0,0 +1,5 @@
+return {
+    override_enabled = true,
+    preset = "RELAXED",  -- Relaxed (No Sweat) --preset = "ENDLESS", -- Endless --preset = "WILDERNESS", -- Wilderness --preset = "COMPLETE_DARKNESS", -- Lights Out
+    overrides = {},
+}
