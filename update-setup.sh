c5eb483b496a6bbb49a5d76640488265ee4baf4c
Sylkyu <miiyusli@gmail.com>
2023-03-18 18:28:06 +1300

    fix update script

--- update-setup.sh
+++ update-setup.sh
@@ -4,17 +4,18 @@ configFiles=("config/cluster.ini" "config/cluster_token.txt")
 
 failed=false
 
+if [ ! -z "$1" ]
+  then
+    echo "Please supply a release version as a parameter e.g. ./update-setup.sh v1.0.0"
+    failed=true
+fi
+
 for file in ${configFiles[@]}; do
   if [ ! -f $file ]; then
     echo "Config file $file does not exist. Please create it."
     failed=true
   fi
 done
-
-if [ -z "$1" ] then
-  echo "Please supply a release version e.g. v1.0.0"
-  failed=true
-fi
 
 if [ $failed ] then
   exit 1
