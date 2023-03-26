2b2baa6b685767e9de2c4c8e2741fc211af15b9b
Sylkyu <miiyusli@gmail.com>
2023-03-26 17:41:29 +1300

    add mods and settings for coop

--- .klei/DoNotStarveTogether/MyDediServer/Master/modoverrides.lua
+++ .klei/DoNotStarveTogether/MyDediServer/Master/modoverrides.lua
@@ -1,8 +1,20 @@
 return {
-    -- ["workshop-000000000"]={
-    --   configuration_options={
-    --     ["CustomModSetting"]="value"
-    --   },
-    --   enabled=true
-    -- },
-  }
+  --  ["workshop-000000000"]={
+  --      configuration_options={
+  --          ["CustomModSetting"]="value"
+  --      },
+  --      enabled=true
+  --  },
+    ["workshop-356930882"] = { enabled = true }, -- Infinite tent uses
+    ["workshop-361994110"] = { enabled = true }, -- Bee Nice
+    ["workshop-454635941"] = { enabled = true }, -- Cats like webber
+    ["workshop-2885308926"] = { enabled = true }, -- Female Webber
+    ["workshop-347079953"] = { enabled = true }, -- Display Food Values
+    ["workshop-1079538195"] = { enabled = true }, -- Moving box
+    ["workshop-375859599"] = { enabled = true }, -- Health Info
+    ["workshop-436654027"] = { enabled = true }, -- Beefalo Milk
+    ["workshop-849986686"] = { enabled = true }, -- Better Pet
+    ["workshop-569043634"] = { enabled = true }, -- Campfire respawn
+    ["workshop-378160973"] = { enabled = true }, -- Global Positions
+    ["workshop-2797670225"] = { enabled = true }, -- Craft From Chest [DST] Fixed
+}
--- .klei/DoNotStarveTogether/MyDediServer/Master/worldgenoverride.lua
+++ .klei/DoNotStarveTogether/MyDediServer/Master/worldgenoverride.lua
@@ -1,5 +1,7 @@
 return {
     override_enabled = true,
     preset = "RELAXED",  -- Relaxed (No Sweat) --preset = "ENDLESS", -- Endless --preset = "WILDERNESS", -- Wilderness --preset = "COMPLETE_DARKNESS", -- Lights Out
-    overrides = {},
+    overrides = {
+        autumn="verylongseason",
+    },
 }
