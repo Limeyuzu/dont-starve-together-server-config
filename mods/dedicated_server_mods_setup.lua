45cb46affca0216714160b51be8ead14444158eb
Sylkyu <miiyusli@gmail.com>
2023-03-26 18:39:44 +1300

    fix mod numbers

--- mods/dedicated_server_mods_setup.lua
+++ mods/dedicated_server_mods_setup.lua
@@ -1,13 +1,13 @@
 --ServerModSetup("000000000") -- description
-ServerModSetup("56930882") -- Infinite tent uses
-ServerModSetup("61994110") -- Bee Nice
-ServerModSetup("54635941") -- Cats like webber
-ServerModSetup("885308926") -- Female Webber
-ServerModSetup("47079953") -- Display Food Values
-ServerModSetup("079538195") -- Moving box
-ServerModSetup("75859599") -- Health Info
-ServerModSetup("36654027") -- Beefalo Milk
-ServerModSetup("49986686") -- Better Pet
-ServerModSetup("69043634") -- Campfire respawn
-ServerModSetup("78160973") -- Global Positions
-ServerModSetup("797670225") -- Craft From Chest [DST] Fixed
+ServerModSetup("356930882") -- Infinite tent uses
+ServerModSetup("361994110") -- Bee Nice
+ServerModSetup("454635941") -- Cats like webber
+ServerModSetup("2885308926") -- Female Webber
+ServerModSetup("347079953") -- Display Food Values
+ServerModSetup("1079538195") -- Moving box
+ServerModSetup("375859599") -- Health Info
+ServerModSetup("436654027") -- Beefalo Milk
+ServerModSetup("849986686") -- Better Pet
+ServerModSetup("569043634") -- Campfire respawn
+ServerModSetup("378160973") -- Global Positions
+ServerModSetup("2797670225") -- Craft From Chest [DST] Fixed
