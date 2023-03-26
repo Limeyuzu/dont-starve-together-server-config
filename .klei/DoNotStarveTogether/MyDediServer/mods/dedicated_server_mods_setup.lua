3482a08625d24b9feef14431eecabf900f318999
Sylkyu <miiyusli@gmail.com>
2023-03-26 18:02:46 +1300

    add mod install script

---
+++ .klei/DoNotStarveTogether/MyDediServer/mods/dedicated_server_mods_setup.lua
@@ -0,0 +1,13 @@
+--ServerModSetup("000000000") -- description
+ServerModSetup("56930882") -- Infinite tent uses
+ServerModSetup("61994110") -- Bee Nice
+ServerModSetup("54635941") -- Cats like webber
+ServerModSetup("885308926") -- Female Webber
+ServerModSetup("47079953") -- Display Food Values
+ServerModSetup("079538195") -- Moving box
+ServerModSetup("75859599") -- Health Info
+ServerModSetup("36654027") -- Beefalo Milk
+ServerModSetup("49986686") -- Better Pet
+ServerModSetup("69043634") -- Campfire respawn
+ServerModSetup("78160973") -- Global Positions
+ServerModSetup("797670225") -- Craft From Chest [DST] Fixed
